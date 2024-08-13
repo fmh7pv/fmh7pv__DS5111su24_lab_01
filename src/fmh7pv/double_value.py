1c584b26ae11903054fce5833c16e4e105994794
fmh7pv <104300517+fmh7pv@users.noreply.github.com>
2024-08-12 22:35:41 -0400

    Create double_value.py

---
+++ src/fmh7pv/double_value.py
@@ -0,0 +1,8 @@
+def double_value(x):
+    """
+    Doubles the given value.
+
+    :param x: The value to be doubled. Can be an integer or float.
+    :return: The doubled value.
+    """
+    return x * 2
